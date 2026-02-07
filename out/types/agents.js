c5d68631aa78dc931f75600bab0f19a9c623f781
Afnan-sh <63289075+Afnan-sh@users.noreply.github.com>
2026-02-08 00:38:56 +0500

    Add files via upload

---
+++ out/types/agents.js
@@ -0,0 +1,6 @@
+"use strict";
+/**
+ * Agent-specific type definitions
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=agents.js.map
---
+++ out/types/index.js
@@ -0,0 +1,6 @@
+"use strict";
+/**
+ * Shared type definitions and interfaces for the extension
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=index.js.map
